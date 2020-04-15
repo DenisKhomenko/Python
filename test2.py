2ef2402be9b66233250113c3be0486be5754e6dd
DenisKhomenko <63407699+DenisKhomenko@users.noreply.github.com>
2020-04-15 18:10:58 +0300

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,12 @@
+# 'Hello world'
+print('Hello world \n')
+# '1__2__4__8__16'
+print(1,2,4,8,16, sep ='__' , end = '\n\n')
+print("#########\n#\t\t#\n#\t\t#\n#\t\t#\n#########\n\n#\t\t#\n#\t\t#\n#########\n#\t\t#\n#\t\t# \n")
+
+
+
+
+
+
+
