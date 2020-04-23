1e46b3a3fbe0dd85b4052220cee7fe481d0fe27c
DenisKhomenko <63407699+DenisKhomenko@users.noreply.github.com>
2020-04-23 20:32:08 +0300

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,10 @@
+#task3
+a = 10
+b= 2
+print('Addition: %d+%d=%d' %(a,b,a+b))
+print('Subtraction: %d-%d=%d ' %(a,b,a-b))
+print('Division: %d+%d=%d ' %(a,b,a/b))
+print('Multiplication: %d*%d=%d ' %(a,b,a*b))
+print('Exponent (Power): %d**%d=%d' %(a,b,a**b))
+print('Modulus: [%d-%d]=%d' %(b,a,abs(b-a)))
+print('Floor division: %d//%d=%d' %(a,b,a//b))
